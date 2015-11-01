7fb1b73bebe52bab51367c71ff6bb240fed699aa
tearne <tearne@gmail.com>
2015-11-01 15:30:58 +0000

    Introduce seaprate reciever things to cope with join

--- shared/src/main/scala/doodle/core/tearne/EventStream.scala
+++ shared/src/main/scala/doodle/core/tearne/EventStream.scala
@@ -1,35 +1,55 @@
 package doodle.core.tearne
 
-sealed trait EventStream[Input, Output] {
-  val downstreamClients = collection.mutable.Set.empty[EventStream[Output, _]]
+trait Reciever[A] {
+  def rx(value: A)
+}
 
-  def map[B](f: Output => B): EventStream[Output, B] = {
-    val downstream = Map(this, f)
-    downstreamClients add downstream
-    downstream
+sealed trait EventStream[Output] {
+  val downstream = collection.mutable.Set.empty[Reciever[Output]]
+
+  def subscribe(node: Reciever[Output]){
+    downstream add node
   }
-  def join[B](that: EventStream[_,B]): EventStream[Nothing,(A,B)] = {
-    val downstream = Join(this, that)
+  
+  def map[B](f: Output => B): EventStream[B] = {
+    val node = Map(this, f)
+    subscribe(node)
+    node
+  }
+  def join[B](that: EventStream[B]): EventStream[(Output,B)] = {
+    val node = Join(this, that)
+    this.subscribe(node.leftReciever)
+    that.subscribe(node.rightReciever)
+    node
     
   }
   //def scan[B](seed: B)(f: (A, B) => B): ES[B] = Scan(this, seed, f)
 
-  def push(value: Input)
+  def push(value: Output){
+    downstream.foreach(_.rx(value))
+  }
 }
 object EventStream {
 }
 
-final case class Map[In, Out](source: EventStream[_, In], f: In => Out) extends EventStream[In, Out] {
-  def push(value: In) = {
-    val result = f(value)
-    downstreamClients.foreach(_ push result)
-  }
-
+final case class Map[In, Out](source: EventStream[In], f: In => Out) 
+    extends Reciever[In] with EventStream[Out]{
+  def rx(value: In) = push(f(value))
 }
-final case class Join[A, B](
-    left: EventStream[_, A],
-    right: EventStream[_, B]) extends EventStream[Nothing, (A, B)] {
-  def push
+
+final case class Join[A, B](left: EventStream[A], right: EventStream[B]) 
+    extends EventStream[(A,B)] {
+  val leftReciever = new Reciever[A]{
+    def rx(value: A) = {
+      ???
+    }
+  }
+  
+  val rightReciever = new Reciever[B]{
+    def rx(value: B) = {
+      ???
+    }
+  }
 }
 
 //final case class Scan[A,B](source: EventStream[A], seed: B, f: (A, B) => B) extends EventStream[B]
